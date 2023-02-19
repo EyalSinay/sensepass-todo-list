5673621e0d3818111533ad405db9cabcbb4f7394
EyalSinay <eyalsinay@gmail.com>
2023-02-19 20:48:50 +0200

    ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,7 +7,8 @@ import { TasksModule } from './task/tasks.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/sensepass-todo-list'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_HOST),
     TasksModule,
   ],
   controllers: [AppController],
